813e62a413baa00b8f7dcd5b74633a241a5f97ff
Bruce <m2deng@ucsd.edu>
2025-01-20 02:52:39 +0000

    Update init.sh

--- init.sh
+++ init.sh
@@ -10,6 +10,13 @@ sudo ln -sf /bin/python3.11 /usr/bin/python
 python spider_agent_setup_snow.py
 python reconstruct_data.py --example_folder examples
 
+gdown 'https://drive.google.com/uc?id=1coEVsCZq-Xvj9p2TnhBFoFTsY-UoYGmG' -O ../../spider2-lite/resource/
+rm -rf ../../spider2-lite/resource/databases/spider2-localdb
+mkdir -p ../../spider2-lite/resource/databases/spider2-localdb
+unzip ../../spider2-lite/resource/local_sqlite.zip -d ../../spider2-lite/resource/databases/spider2-localdb
+python spider_agent_setup_lite.py
+python reconstruct_data.py --example_folder examples_lite
+
 cd ~/results-log
 python get_subset.py
 
