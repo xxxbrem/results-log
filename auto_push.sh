c5149d4106e51be7696102cb3e1a3b092de94696
Bruce <m2deng@ucsd.edu>
2025-01-23 01:50:57 +0000

    Update auto_push

--- auto_push.sh
+++ auto_push.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 git config --global user.email "m2deng@ucsd.edu"
 git config --global user.name "Bruce"
-cp -r ../snow-spider-agent/methods/spider-self-refine/output ./
+
+rm -rf ./output
+mkdir -p ./output
+find ../snow-spider-agent/methods/spider-self-refine/output/ -type d -name "*-log" -exec cp -r {} ./output \;
 
 TARGET_DIR="./" 
 COMMIT_MESSAGE="Auto commit and push at $(date +"%Y-%m-%d %H:%M:%S")"
